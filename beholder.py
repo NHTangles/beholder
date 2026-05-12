12e86bb54f0ec1e3b024d11f77478c1c13f79826
k21971 <keith.simpson1971@gmail.com>
2026-05-12 19:57:59 -0400

    Support for latest version of CrecelleHack (1.5.0).

--- beholder.py
+++ beholder.py
@@ -238,7 +238,7 @@ class DeathBotProtocol(irc.IRCClient):
                  filepath.FilePath(FILEROOT+"nethackathon/var/xlogfile"): ("nhthon", "\t", "nethackathon/dumplog/{starttime}.nhthon.html"),
                  filepath.FilePath(FILEROOT+"tnnt/var/xlogfile"): ("tnnt", "\t", "tnnt/dumplog/{starttime}.tnnt.html"),
                  filepath.FilePath(FILEROOT+"nerfhack-2.2.1/var/xlogfile"): ("nerf", "\t", "nerfhack/dumplog/{starttime}.nerf.html"),
-                 filepath.FilePath(FILEROOT+"crecellehack-1.4.2/var/xlogfile"): ("cre", "\t", "crecellehack/dumplog/{starttime}.cre.html"),
+                 filepath.FilePath(FILEROOT+"crecellehack-1.5.0/var/xlogfile"): ("cre", "\t", "crecellehack/dumplog/{starttime}.cre.html"),
                  filepath.FilePath(FILEROOT+"unnethack-6.0.14/var/xlogfile"): ("un", "\t", "unnethack/dumplog/{starttime}.un.txt.html")}
     livelogs  = {filepath.FilePath(FILEROOT+"nh343-hdf/var/livelog"): ("nh343", ":"),
                  filepath.FilePath(FILEROOT+"nh363-hdf/var/livelog"): ("nh363", "\t"),
@@ -261,7 +261,7 @@ class DeathBotProtocol(irc.IRCClient):
                  filepath.FilePath(FILEROOT+"acehack/livelog"): ("ace", ":"),
                  filepath.FilePath(FILEROOT+"hackem-1.3.2/var/livelog"): ("hackm", "\t"),
                  filepath.FilePath(FILEROOT+"nerfhack-2.2.1/var/livelog"): ("nerf", "\t"),
-                 filepath.FilePath(FILEROOT+"crecellehack-1.4.2/var/livelog"): ("cre", "\t"),
+                 filepath.FilePath(FILEROOT+"crecellehack-1.5.0/var/livelog"): ("cre", "\t"),
                  filepath.FilePath(FILEROOT+"unnethack-6.0.14/var/livelog"): ("un", "\t")}
 
     # Forward events to other bots at the request of maintainers of other variant-specific channels
@@ -458,7 +458,7 @@ class DeathBotProtocol(irc.IRCClient):
                           INPR+"nerf221/"],
                  "cre" : [INPR+"cre100/", INPR+"cre101/",
                           INPR+"cre122/", INPR+"cre140/",
-                          INPR+"cre142/"],
+                          INPR+"cre142/", INPR+"cre150/"],
                  "dyn" : [INPR+"dyn/"]}
 
     # for !whereis
@@ -646,7 +646,8 @@ class DeathBotProtocol(irc.IRCClient):
                          FILEROOT+"crecellehack-1.0.1/var/whereis/",
                          FILEROOT+"crecellehack-1.2.2/var/whereis/",
                          FILEROOT+"crecellehack-1.4.0/var/whereis/",
-                         FILEROOT+"crecellehack-1.4.2/var/whereis/"],
+                         FILEROOT+"crecellehack-1.4.2/var/whereis/",
+                         FILEROOT+"crecellehack-1.5.0/var/whereis/"],
                   "un": [FILEROOT+"un531/var/unnethack/",
                          FILEROOT+"un532/var/unnethack/",
                          FILEROOT+"unnethack-6.0.0/var/unnethack/",
